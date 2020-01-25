4ece945175d250f9ad1fdce477b2841f7c72cda1
Lukas Masuch <lukas.masuch@gmail.com>
2020-01-25 01:45:16 +0000

    Split parse code into method

--- best_of/generator.py
+++ best_of/generator.py
@@ -9,6 +9,33 @@ import pandas as pd
 log = logging.getLogger(__name__)
 
 
+def parse_projects_yaml(projects_yaml_path: str):
+    from best_of import projects_collection
+    
+    parsed_yaml = {}
+    
+    # https://docs.ansible.com/ansible/latest/reference_appendices/YAMLSyntax.html
+    # https://github.com/Animosity/CraftIRC/wiki/Complete-idiot%27s-introduction-to-yaml
+    
+    if not os.path.exists(projects_yaml_path):
+            raise Exception("Projects yaml file does not exist: " +
+                            os.path.abspath(projects_yaml_path))
+    with open(projects_yaml_path, 'r') as stream:
+            parsed_yaml = yaml.safe_load(stream)
+    
+    projects = parsed_yaml["projects"]
+    
+    config = projects_collection.prepare_configuration(
+            parsed_yaml["configuration"] if "configuration" in parsed_yaml else {})
+
+    categories = projects_collection.prepare_categories(
+            parsed_yaml["categories"] if "categories" in parsed_yaml else [])
+
+    labels = parsed_yaml["labels"] if "labels" in parsed_yaml else []
+    
+    return config, projects, categories, labels
+
+
 def generate_markdown(projects_yaml_path: str, libraries_api_key: str, github_api_key: str = None):
     try:
         # Set libraries api key
@@ -22,28 +49,9 @@ def generate_markdown(projects_yaml_path: str, libraries_api_key: str, github_ap
 
         # Needs to be imported without setting environment variable
         from best_of import md_generation, projects_collection
-        parsed_yaml = {}
-
-        # https://docs.ansible.com/ansible/latest/reference_appendices/YAMLSyntax.html
-        # https://github.com/Animosity/CraftIRC/wiki/Complete-idiot%27s-introduction-to-yaml
-
-        if not os.path.exists(projects_yaml_path):
-            raise Exception("Projects yaml file does not exist: " +
-                            os.path.abspath(projects_yaml_path))
-
-        with open(projects_yaml_path, 'r') as stream:
-            parsed_yaml = yaml.safe_load(stream)
-
-        projects = parsed_yaml["projects"]
-
-        config = projects_collection.prepare_configuration(
-            parsed_yaml["configuration"] if "configuration" in parsed_yaml else {})
-
-        categories = projects_collection.prepare_categories(
-            parsed_yaml["categories"] if "categories" in parsed_yaml else [])
-
-        labels = parsed_yaml["labels"] if "labels" in parsed_yaml else []
-
+        
+        config, projects, categories, labels = parse_projects_yaml(projects_yaml_path)
+        
         projects = projects_collection.collect_projects_info(
             projects, categories, config)
 
